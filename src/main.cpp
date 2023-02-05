1235e9581f678b72fd89b2377e5b2ebe63f5c1c2
Crparichaya <parichaya.yan@ku.th>
2023-02-05 14:31:42 +0700

    Finished

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Bounce2.h>
 #define BUTTON 27
 #define GREEN 33
-#define RED 23
+#define RED 26
 #define YELLOW 25
 #define LDR 32
 int cnt = 0;
@@ -19,27 +19,59 @@ void setup() {
 
 
     ledcSetup(0, 5000, 8);
-    ledcAttachPin(GREEN, 0);
+    ledcSetup(1,5000,8);
+    ledcSetup(2,5000,8);
+    ledcAttachPin(GREEN, 2);
     ledcAttachPin(RED,0);
-    ledcAttachPin(YELLOW,0);
+    ledcAttachPin(YELLOW,1);
+    
 }
 
 void loop() {
     debouncer.update();
     if ( debouncer.fell() ) { 
         cnt++;
-        Serial.println(cnt);
-    }
+
     if(cnt>0b111){
         cnt=0b000;
+      
     }
-
-    int value = map(analogRead(LDR),0,4095,0,255);
-
-
-    ledcWrite(0, cnt&0b100==0b100?value:0);//RED
-    ledcWrite(1, cnt&0b010==0b010?value:0);//YELLOW
-    ledcWrite(2, cnt&0b001==0b001?value:0);//
+    Serial.println(cnt);
+    }
     
 
+    int value = map(analogRead(LDR),0,4095,0,255);
+    Serial.println(value);
+    delay(100);
+
+
+    ledcWrite(0, (cnt&0b100)==0b100?value:0);//RED
+    ledcWrite(1, (cnt&0b010)==0b010?value:0);//YELLOW
+    ledcWrite(2, (cnt&0b001)==0b001?value:0);//
+    
 }
+
+// #define GREEN 33
+
+// #define YELLOW 25
+// #define RED 26
+// void setup() {
+//     pinMode(RED, OUTPUT);
+//     digitalWrite(RED,0);
+//     pinMode(YELLOW, OUTPUT);
+//     digitalWrite(YELLOW,0);
+//     pinMode(GREEN, OUTPUT);
+//     digitalWrite(GREEN,0);
+// }
+
+// void loop() {
+//     digitalWrite(RED,1);
+//     digitalWrite(YELLOW,1);
+//     digitalWrite(GREEN,1);
+//     delay(500);
+
+//     digitalWrite(RED,0);
+//     digitalWrite(YELLOW,0);
+//     digitalWrite(GREEN,0);
+//     delay(500);
+// }
