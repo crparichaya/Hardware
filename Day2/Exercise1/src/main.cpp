d5ecf18608687198bacc109feac1172315886cba
VeryHardBit <yanothaic15@gmail.com>
2023-02-07 11:00:32 +0700

    ex1 LED

--- Day2/Exercise1/src/main.cpp
+++ Day2/Exercise1/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+#define RED 12 
+
+void task_LED(void *param){
+    while(1){
+        int v=sin(millis()/200.0)*127+128;
+        analogWrite(RED, v);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+}
+TaskHandle_t TaskLED=NULL;
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(RED,OUTPUT);
+    xTaskCreatePinnedToCore(task_LED, "task_LED", 1000, NULL, 1, &TaskLED, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
