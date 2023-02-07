0dcf8b382081c967b8c4601ab1719856fe4637a5
VeryHardBit <yanothaic15@gmail.com>
2023-02-07 16:00:02 +0700

    Post_Task3

--- Day2/Exercise3/src/main.cpp
+++ Day2/Exercise3/src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <Bounce2.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
 
 #define GREEN 27
 #define BUTTON 26
@@ -35,11 +38,61 @@ void Task2(void *param){
     }
 }
 
+void GET_Task3();
+int a,b;
+String op,questionId;
+TaskHandle_t Task3_t = NULL;
+void Task3(void *param){
+    int time0=millis();
+    while(1){
+        if(millis()-time0>5000){
+            GET_Task3();
+            POST_Task3();
+            time0=millis();
+        }
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+}
+void POST_Task3(){
+    String url="https://exceed-hardware-stamp465.koyeb.app/answer";
+    String json;
+    HTTPClient http;
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+
+
+    DynamicJsonDocument doc(2048);
+    doc["questionId"]=questionId;
+    if(doc["op"]=="+"){
+        doc["result"]=a+b;
+    }
+    if(doc["op"]=="-"){
+        doc["result"]=a-b;
+    }
+    if(doc["op"]=="*"){
+        doc["result"]=a*b;
+    }
+    if(doc["op"]=="/"){
+        doc["result"]=a/b;
+    }
+    serializeJson(doc,json);
+    int httpCode = http.POST(json);
+    if(httpCode/100==2){
+        Serial.println(http.getString());
+    }else{
+        Serial.println("Error somehow lol");
+        Serial.println(httpCode);
+        Serial.println(http.getString());
+    }
+
+}
+
 void setup(){
     Serial.begin(115200);
     pinMode(GREEN, OUTPUT);
     xTaskCreatePinnedToCore(Task1, "Task1", 1024*10, NULL, 1, &Task1_t, 0);
     xTaskCreatePinnedToCore(Task2, "Task2", 1000, NULL, 1, &Task2_t, 0);
+    xTaskCreatePinnedToCore(Task3, "Task3", 1024*10, NULL, 1, &Task3_t, 0);
     debouncer.attach(BUTTON, INPUT_PULLUP);
     debouncer.interval(26);
     pinMode(BLUE, OUTPUT);
