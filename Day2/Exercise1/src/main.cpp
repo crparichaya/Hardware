8e1b74ff3e46cf8ac3398f1c478033948c0615a5
Crparichaya <parichaya.yan@ku.th>
2023-02-07 11:18:14 +0700

    fixed

--- Day2/Exercise1/src/main.cpp
+++ Day2/Exercise1/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <Bounce2.h>
 #define BUTTON 26
-
+Bounce debouncer = Bounce();
+int cnt = 0;
 
 #define RED 12 
 
@@ -12,25 +13,29 @@ void task_LED(void *param){
         vTaskDelay(10/portTICK_PERIOD_MS);
     }
 }
+
+TaskHandle_t TaskButton=NULL;
+void Task_Button(void *param){
+    while(1){
+        debouncer.update();
+        if (debouncer.fell()){
+          cnt++;
+          Serial.println(cnt);
+        }
+    }
+}
+
 TaskHandle_t TaskLED=NULL;
 void setup() {
     pinMode(RED,OUTPUT);
     xTaskCreatePinnedToCore(task_LED, "task_LED", 1000, NULL, 1, &TaskLED, 0);
+    xTaskCreatePinnedToCore(Task_Button, "task_Button", 1000, NULL, 1, &TaskLED, 0);
+    debouncer.attach(BUTTON, INPUT_PULLUP);
+    debouncer.interval(26);
 }
 
 void loop(){
 
 
-}
-
-Bounce debouncer = Bounce();
-
-void task_LED(void *param){
-    while(1){
-        debouncer.update();
-        if (debouncer.fell()){
-          cnt++
-        }
-    }
 }
 
