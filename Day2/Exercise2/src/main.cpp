4567d0f52a46c2bd056224727d1cba3dbbe387ac
VeryHardBit <yanothaic15@gmail.com>
2023-02-07 14:02:40 +0700

    increase stack size

--- Day2/Exercise2/src/main.cpp
+++ Day2/Exercise2/src/main.cpp
@@ -1,14 +1,44 @@
 #include <Arduino.h>
-#include <Request.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
 
 
 
-#define RED 12 
+#define RED 12
+ 
 TaskHandle_t RedLight = NULL;
 TaskHandle_t delayy = NULL;
 int the_delay = 500;
-int the_time = 0;
+int time0 = 0;
 
+
+const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app";
+
+int GET_Delay(){
+    DynamicJsonDocument doc(2048);
+    HTTPClient http;
+    const String url=baseUrl+"/leddelay";
+    http.begin(url);
+    Serial.println("Getting Delay value");
+    int delay=-1;
+    int httpCode = http.GET();
+    if (httpCode>=200&&httpCode<300) {
+        String payload = http.getString();
+        deserializeJson(doc, payload);
+        delay = doc["value"];
+    }
+    return delay;
+}
+void Connect_Wifi(){
+    WiFi.begin("OPPO_KUY", "oppopass");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    Serial.println(WiFi.localIP());
+}
 
 void Blink_LED(void *param){
     while(1){
@@ -20,8 +50,8 @@ void Blink_LED(void *param){
 }
 void Task_delay(void *param){
     while(1){
-        if(millis()-the_time>5000){
-            the_time = millis();
+        if(millis()-time0>5000){
+            time0 = millis();
             the_delay = GET_Delay();
             Serial.println(the_delay);
         }
@@ -33,8 +63,8 @@ void setup(){
     Serial.begin(115200);
     pinMode(RED, OUTPUT);
     Connect_Wifi();
-    xTaskCreatePinnedToCore(Blink_LED, "task_LED", 1000, NULL, 1, &RedLight, 0);
-    xTaskCreatePinnedToCore(Task_delay, "task_DELAY", 1000, NULL, 1, &delayy, 1);
+    xTaskCreatePinnedToCore(Blink_LED, "task_LED", 1024*10, NULL, 1, &RedLight, 0);
+    xTaskCreatePinnedToCore(Task_delay, "task_DELAY", 1024*10, NULL, 1, &delayy, 0);
 }
 
 void loop() {
