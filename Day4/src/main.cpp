ebea7b770418a5c1cc80bda255223ce0e6233142
VeryHardBit <yanothaic15@gmail.com>
2023-02-08 11:47:18 +0700

    Finding pin

--- Day4/src/main.cpp
+++ Day4/src/main.cpp
@@ -1,29 +1,34 @@
 // ESP32 Touch Test
 // Just test touch pin - Touch0 is T0 which is on GPIO 4.
 #include <Arduino.h>
-// void setup()
-// {
-//   Serial.begin(115200);
-//   delay(1000); // give me time to bring up serial monitor
-// }
-// void loop()
-// {
-//   Serial.println(touchRead(T0));  // get value using T0
-// }
-
-const int ledPin =  26;      // the number of the LED pin
-const int threshold =  40;      // the number of the LED pin
-int ledState = LOW;             // ledState used to set the LED
-void setup() {
-  // put your setup code here, to run once:
-  //pinMode(ledPin, OUTPUT);
+//T0 not work
+//T3 work
+void setup()
+{
   Serial.begin(115200);
-  touchAttachInterrupt(T7 , click , threshold );
+  delay(1000); // give me time to bring up serial monitor
 }
-void loop() {    
-  Serial.println( touchRead(T) );
-  delay(200);
+void loop()
+{
+  Serial.print(touchRead(T3));  // get value using T0
+  Serial.print(" ");
+  Serial.println(millis());
+  delay(100);
 }
-void click () {
-  Serial.println("click!");
-}
+
+//const int ledPin =  26;      // the number of the LED pin
+//const int threshold =  40;      // the number of the LED pin
+//int ledState = LOW;             // ledState used to set the LED
+//void setup() {
+//  // put your setup code here, to run once:
+//  //pinMode(ledPin, OUTPUT);
+//  Serial.begin(115200);
+//  touchAttachInterrupt(T7 , click , threshold );
+//}
+//void loop() {    
+//  Serial.println( touchRead(T) );
+//  delay(200);
+//}
+//void click () {
+//  Serial.println("click!");
+//}
